b0f7d932fc17bb2d6493bdf89c5cc5dd3034021f
jenilparsania <151541448+jenilparsania@users.noreply.github.com>
2025-10-04 16:24:04 -0300

    closures

--- revise_0.js
+++ revise_0.js
@@ -1,1 +1,16 @@
 // closures
+//A closure is a function that retains access to its lexical scope, 
+// even when the function is executed outside that scope.
+// Use Cases : Data Privacy, Memoziation,Currying and Maintaining state
+function outer(){
+    let count = 1;
+    return function inner(){
+        count*=4;
+        return count;
+    };
+
+}
+
+const counter = outer();
+console.log(counter()); // 4
+console.log(counter()); // 16

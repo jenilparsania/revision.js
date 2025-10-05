7066825d8168dc55d978d74a8847f3fbed627452
jenilparsania <151541448+jenilparsania@users.noreply.github.com>
2025-10-05 15:43:26 -0300

    methods

--- methods.js
+++ methods.js
@@ -10,3 +10,11 @@ const person = {
 }
 
 console.log(person.fullName());
+/*
+this : Alone refers to the global object
+in a function : this refers to the global object
+in a function in a strict mode : this is undefined
+in an object, this refers to the object
+in an event, this refers to the element that received the event
+Methods like call(),apply(), and bind() can refer this to any object
+*/

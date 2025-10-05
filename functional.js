afbe9ea0462efe5c906fdf535279bcf6228e11f2
jenilparsania <151541448+jenilparsania@users.noreply.github.com>
2025-10-05 15:04:13 -0300

    understanding functional programming_2

---
+++ functional.js
@@ -0,0 +1,18 @@
+
+
+// declarative-programming paradigm : if the language follows the declarative paradigm,we write the code that specifies what you want to do , without saying how.
+
+// imperative programming paradigm : if the language follows the imperative/procedural paradigm, we write the code that follows how to do something.
+
+
+// JS is multi-paradigm language. 
+
+//functional_programming
+// because it is a sub-paradigm form the declarative paradigm, this affexts the way you write functional code. it generally leads to less code,because JS already has a lot of the in-built functions we commonly need.
+
+// 2 laws to be followed to understand the functional programming 
+// 1) you architect your software out of pure, isolated functions
+// 2) you avoid mutability and side-effects
+
+
+
